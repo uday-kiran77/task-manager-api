f44287a5b6dc04d78816e0d20683ca843fc3662d
Uday kiran <64728986+uday-kiran77@users.noreply.github.com>
2021-08-03 21:21:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const userRouter=require('./routes/user')
 const taskRouter=require('./routes/task')
 
 const app=express()
-const port=process.env.PORT
+const port=process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
